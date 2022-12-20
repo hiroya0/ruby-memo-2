7b07b5a2d22f15f0c84cdbe824afa25b031205a0
EC2 Default User <ec2-user@ip-172-31-14-120.ap-northeast-1.compute.internal>
2022-12-20 12:46:06 +0000

    ruby課題_メモ

---
+++ memo.rb
@@ -0,0 +1,37 @@
+require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
+
+puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+
+memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
+
+# if文を使用して続きを作成していきましょう。
+# 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
+if memo_type  == 1
+  
+    puts "拡張子を除いたファイルを作成してください"
+input_file = gets.chomp
+    puts "メモしたい内容を記入してください"
+    puts "完了したらCtrl + Dを押します"
+  
+memo = STDIN.read
+   
+  CSV.open("#{input_file}.csv","w") do |csv|
+     
+  csv.puts["#{memo}"]
+  end
+  
+elsif memo_type == 2
+    puts "既存のメモを編集します。拡張子を除いたファイルを入力してください"
+  input_file = gets.chomp
+     puts "メモしたい内容を記入してください"
+    puts "完了したらCtrl + Dを押します"
+  
+memo = STDIN.read
+   
+  CSV.open("#{input_file}.csv","a") do |csv|
+     
+  csv.puts["#{memo}"]
+  end
+end
+    
+  
